6f93296b4c843228fdd4420147b2045e305caa29
SonOfSauce <johnharms0404@gmail.com>
2025-04-09 10:51:24 -0500

    Finished Aync Practice

---
+++ Microsoft_Back_End_Developer/Module 2/AsyncFunction/Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+public class Program
+{
+    async Task DownloadDataAsync()
+    {
+        try
+        {
+            throw new Exception("Whoops");
+            await Task.Delay(500);
+            Console.WriteLine("finished task");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error Here {ex.Message}");
+        }
+    }
+
+    async Task DownloadDataAsync2()
+    {
+        await Task.Delay(900);
+        Console.WriteLine("finished task2");
+    }
+
+    public static async Task Main()
+    {
+        Program program = new Program();
+        await Task.WhenAll(program.DownloadDataAsync(), program.DownloadDataAsync2());
+        Console.WriteLine("Finished Both Thingies");
+    }
+}
